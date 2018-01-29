cbd942c88a72f07a1e01a0586584df292d75afc8
wjepower <wjepower@hotmail.com>
2018-01-29 23:35:57 +0800

    20180129 tiaozhan1 OK

--- one_day/calculator.py
+++ one_day/calculator.py
@@ -9,17 +9,17 @@ else:
         b = int(sys.argv[1])
         a = b - 3500
         if a <= 1500:
-            print('{}'.format(a*0.03, '.2f'))
+            print(format(a*0.03, '0.2f'))
         elif a > 1500 and a <= 4500:
-            print('{}'.format(a*0.1-105, '.2f'))
+            print('{:.2f}'.format(a*0.1-105))
         elif a > 4500 and a <= 9000:
-            print('{}'.format(a*0.2-555, '.2f'))
+            print('{:.2f}'.format(a*0.2-555))
         elif a > 9000 and a <= 35000:
-            print('{}'.format(a*0.25-1005, '.2f'))
+            print('{:.2f}'.format(a*0.25-1005))
         elif a > 35000 and a <= 55000:
-            print('{}'.format(a*0.3-2755, '.2f'))
+            print('{:.2f}'.format(a*0.3-2755))
         elif a > 55000 and a <= 80000:
-            print('{}'.format(a*0.35-5505, '.2f'))
+            print('{:.2f}'.format(a*0.35-5505))
         else:
             print('{}'.format(a*0.45-13505, '.2f'))
     except:
